f0fd27a818a34905de27b6d4ee50c403ee81bd90
John Doe <rabinarahman@gmail.com>
2022-05-14 12:04:05 -0700

    User Route Created

---
+++ backend/router/userRoutes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router()
+const asyncHandler = require("express-async-handler")
+const User = require("../model/User")
+var bcrypt = require('bcryptjs');
+
+router.post("/signin", asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    const user = await User.findOne({ email: email }).select("-__v -createdAt -updatedAt")
+    if (user) {
+        const match = bcrypt.compareSync(password, user.password)
+        if (match) {
+            return res.send({ ...user._doc })
+        }
+    }
+    return res.status(401).send({ message: 'Invalid email or password' });
+}))
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,23 @@ const express = require("express")
 const data = require("./data")
 const cors = require("cors")
 const mongoose = require("mongoose")
-const router=require("./router/seedProduct")
-const productRoutes=require("./router/productRoutes")
+const router = require("./router/seedProduct")
+const productRoutes = require("./router/productRoutes")
+const userRouters=require("./router/userRoutes")
 const app = express()
 const URI = process.env.MONGODB_URI
 app.use(cors())
-
+app.use(express.json())
 app.use("/api/seed", router)
 app.use("/api/product", productRoutes)
-
-
+app.use("/api/user", userRouters)
+app.use((err, req, res, next) => {
+    res.status(500).send({status:err, message: err.message })
+})
 
 const connectDB = async () => {
     try {
-        
+
         const conn = await mongoose.connect(URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
