7aa413a2ea85c5bb9518cd254656e67e2718b838
Rasel Hossain <rabinarahman@gmail.com>
2022-05-31 11:07:41 -0700

    Create IsAuth Middleware

--- backend/utils.js
+++ backend/utils.js
@@ -4,6 +4,8 @@ const generateToken = (user) => {
     return jwt.sign(user, JWT_SECRET, { expiresIn: '30d' })
 }
 
-
+const isAuth = (req, res, next) => {
+    const authorization = req.headers.authorization
+}
 
 module.exports = { generateToken }
