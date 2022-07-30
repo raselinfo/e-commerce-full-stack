300c494218b1ced92178e9fae357d295ab4d8080
Rasel Hossain <rabinarahman@gmail.com>
2022-07-30 05:35:41 -0700

    Dropzoon Complete

--- frontend/src/views/SignIn.js
+++ frontend/src/views/SignIn.js
@@ -7,7 +7,7 @@ const SignIn = () => {
   return (
     <div>
       <div className="form__wrapper shadow-lg p-5  lg:w-2/6 md:w-3/6 w-5/6 rounded-lg">
-        <h3 className="text-5xl font-bold my-10">
+        <h3 className="text-5xl font-bold my-10 text-center">
           Sign
           <span className="text-yellow-500"> In</span>
         </h3>
--- frontend/src/views/SignUp.js
+++ frontend/src/views/SignUp.js
@@ -1,16 +1,45 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { Link } from "react-router-dom";
 import getQueryString from "../utils/getQueryString";
+import { useDropzone } from "react-dropzone";
 const SignIn = () => {
   const { redirect } = getQueryString(["redirect"]);
   const [isShowPass, setIsShowPass] = useState(false);
+  const [image, setImage] = useState("");
+  const [isError, setIsError] = useState("");
+  const onDrop = useCallback((acceptedFiles, rejectedFiles) => {
+    setIsError("");
+    setImage({});
+    const reader = new FileReader();
+    if (acceptedFiles.length) {
+      reader.onload = () => {
+        setImage(reader.result);
+      };
+      reader.readAsDataURL(acceptedFiles[0]);
+    }
+
+    if (rejectedFiles.length) setIsError(rejectedFiles[0].errors[0].message);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: {
+      "image/jpg": [".jpg", ".png"],
+    },
+    maxFiles: 1,
+    maxSize: 2e6,
+  });
+
   return (
     <div>
       <div className="form__wrapper shadow-lg p-5 mt-5  lg:w-2/6 md:w-3/6 w-5/6 rounded-lg">
-        <h3 className="text-5xl font-bold my-10">
+        <h3 className="text-5xl font-bold my-10 text-center">
           Sign
           <span className="text-yellow-500"> UP</span>
         </h3>
+        <div className="image__wrapper">
+          {image && <img className="h-32 mt-3 avater" src={image} alt="" />}
+        </div>
         <form className="">
           <div className="input__group mb-3">
             <label className="block text-2xl" htmlFor="name">
@@ -28,13 +57,18 @@ const SignIn = () => {
             <label className="block text-2xl" htmlFor="image">
               Image<span className="text-red-500 font-bold">*</span>
             </label>
-            <input
-              className="block shadow-md py-4 px-5 text-lg font-bold rounded-lg w-full"
-              type="file"
-              name="image"
-              id="image"
-              placeholder="Enter your Name"
-            />
+            <div
+              {...getRootProps()}
+              className="border-dashed border-2 py-4 px-5 border-gray-500 mt-2 rounded-lg bg-red-50"
+            >
+              <input {...getInputProps()} />
+              {isDragActive ? (
+                <p>Drop the file here....</p>
+              ) : (
+                <p>Drop One image here.or click to select files</p>
+              )}
+            </div>
+            <p>{isError}</p>
           </div>
           <div className="input__group">
             <label className="block text-2xl" htmlFor="email">
@@ -63,9 +97,11 @@ const SignIn = () => {
               Show Password
             </label>
             <input
-              onChange={(e) => setIsShowPass(prev=>{
-                return !prev
-              })}
+              onChange={(e) =>
+                setIsShowPass((prev) => {
+                  return !prev;
+                })
+              }
               type="checkbox"
               name="show"
               id="show"
@@ -96,7 +132,7 @@ const SignIn = () => {
                 className="text-blue-600 underline"
                 to={`/signin?redirect=${redirect}`}
               >
-                Sign Up
+                Sign In
               </Link>
             </p>
           </div>
