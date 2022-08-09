ecd0dafce8d7a32ee2931f9c65ba3fe7469ea655
Rasel Hossain <rabinarahman@gmail.com>
2022-08-09 04:06:16 -0700

    style: sizeVarient text spell fiex In AuthSocialIcons component i fixed sizeVarient spell

--- frontend/src/components/AuthSocialIcons.js
+++ frontend/src/components/AuthSocialIcons.js
@@ -2,7 +2,7 @@ import React from "react";
 import facebook from "../assets/images/icons/facebook.png";
 import google from "../assets/images/icons/google.png";
 import github from "../assets/images/icons/github.png";
-const sizeVarient = {
+const sizeVariant = {
   small: "w-10",
   medium: "w-16",
   large: "w-3xl",
@@ -17,19 +17,19 @@ const AuthSocialIcons = ({
     <div className="flex gap-6 align-middle justify-center">
       <img
         onClick={facebookHandler}
-        className={`${sizeVarient[size]} cursor-pointer`}
+        className={`${sizeVariant[size]} cursor-pointer`}
         src={facebook}
         alt="facebook"
       />
       <img
         onClick={googleHandler}
-        className={`${sizeVarient[size]} cursor-pointer`}
+        className={`${sizeVariant[size]} cursor-pointer`}
         src={google}
         alt="google"
       />
       <img
         onClick={githubHandler}
-        className={`${sizeVarient[size]} cursor-pointer`}
+        className={`${sizeVariant[size]} cursor-pointer`}
         src={github}
         alt="github"
       />
