e5d425769c3ad041d33b7f0cf3cb4c88f292ce97
Rasel Hossain <rabinarahman@gmail.com>
2022-09-13 23:15:41 +0600

    feat: Payment Scree Payment screen created in frontend Payment.js

--- frontend/src/views/Payment.js
+++ frontend/src/views/Payment.js
@@ -1,11 +1,15 @@
-import React from 'react';
-
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Store } from "../Store/Store";
 const Payment = () => {
-    return (
-        <div>
-            Payment
-        </div>
-    );
+  const { state } = useContext(Store);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!state.userInfo.email) {
+      navigate("/checkout?step=shipping", { replace: true });
+    }
+  }, [navigate, state.userInfo.email]);
+  return <div>Payment</div>;
 };
 
 export default Payment;
