ca71b91d6090db906fb01c460ddaed7ca6ea7944
Rasel Hossain <rabinarahman@gmail.com>
2022-11-26 01:03:55 +0600

    Bugs

--- backend/routes/resetPassword/resetPassRoutes.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const resetPassController = require("../../controller/resetPassword/resetPassController");
-// Reset Password
-router.post("/reset/password/:token", resetPassController);
-
-module.exports = router;
--- backend/routes/verify/emailVerifyRoutes.js
+++
@@ -1,6 +0,0 @@
-const verifyEmailController = require("../../controller/verify/verifyEmailController");
-
-const router = require("express").Router();
-
-router.get("/verify/email/:email/:token", verifyEmailController);
-module.exports = router;
