3b2a969365d8f0de3abf8510ac3b9a83eca2cd52
Rasel Hossain <rabinarahman@gmail.com>
2022-11-28 22:46:29 +0600

    (fix:Password.js): fixe the path name

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,7 @@
 import Axios from "axios";
+console.log(process.env.REACT_APP_SERVER_URL);
 const axios = Axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: process.env.REACT_APP_SERVER_URL,
 });
 
 export default axios;
