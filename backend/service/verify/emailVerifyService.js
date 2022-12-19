0b67dc4136ec6b822a2366c55c3dd1a8dae893a9
Rasel Hossain <rabinarahman@gmail.com>
2022-12-19 20:06:39 +0600

    refector(forgotPassword.js): rename jwt signAccessToken and signrefreshToken

--- backend/service/verify/emailVerifyService.js
+++ backend/service/verify/emailVerifyService.js
@@ -1,21 +1,20 @@
-const User = require("../../model/User");
-const Token = require("../../model/Token");
-const JwtService = require("../jwt/JWT");
+const User = require('../../model/User');
+const Token = require('../../model/Token');
+const JwtService = require('../jwt/JWT');
 const emailVerify = async (userToken, email) => {
   try {
-    
-    await JwtService.verify(userToken);
+    await JwtService.verifyAccessToken(userToken);
     const isValidUser = await User.findOne({ email });
     const isValidToken = await Token.findOne({ email, token: userToken });
     if (!isValidUser || !isValidToken) {
-      return { error: { message: "Not valid email" } };
+      return { error: { message: 'Not valid email' } };
     }
     isValidUser.verified = true;
     const updatedUser = await isValidUser.save();
     if (updatedUser) {
       // const deleteToken = await isValidToken.remove();
       const deleteToken = await Token.remove({ email: email });
-      if (deleteToken) return { success: "OK" };
+      if (deleteToken) return { success: 'OK' };
     }
   } catch (err) {
     return { error: { message: err.message } };
