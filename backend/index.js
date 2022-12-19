3b452960e14bfcda6784239ce33cba9d294ed60b
Rasel Hossain <rabinarahman@gmail.com>
2022-12-19 17:58:42 +0600

    feat(index.js): Add cookie parser package and create a test api

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 // Passport
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const logger = require('./utils/logger');
 const connectDB = require('./db/db');
@@ -11,6 +12,7 @@ const morgan = require('morgan');
 morgan('tiny');
 
 // Middleware
+app.use(cookieParser());
 app.use(express.json({ limit: 10000000000 }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -51,6 +53,19 @@ app.use('/api/v1', require('./routes/verify/emailVerifyRoutes'));
 app.use('/api/v1', require('./routes/forgotPassword/forgotPasswordRoutes'));
 app.use('/api/v1', require('./routes/resetPassword/resetPassRoutes'));
 app.use('/api/v1', require('./routes/Charge/shippingChargeRoutes'));
+app.use('/api/v1', require('./routes/auth/refreshTokenRoute'));
+app.use('/api/v1', require('./routes/auth/logoutRoutes'));
+
+// Protected Route
+app.post(
+  '/api/v1/test',
+  require('./middleware/auth/user.middleware'),
+  require('./middleware/auth/authentication.middleware'),
+  (req, res) => {
+    console.log('hello test');
+    res.send('Test is ok');
+  }
+);
 
 // Todo: Health Route
 app.get('/api/v1/health', (req, res) => {
