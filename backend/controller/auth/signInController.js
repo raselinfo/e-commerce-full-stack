31887ee5ac089da184fe200a6971120e0e72367e
Rasel Hossain <rabinarahman@gmail.com>
2022-12-20 13:35:09 +0600

    bug(http-only cookie): test why it's not work on live server

--- backend/controller/auth/signInController.js
+++ backend/controller/auth/signInController.js
@@ -12,16 +12,19 @@ const signInController = async (req, res, next) => {
     }
 
     console.log('check cookie', res);
-    return res
-      .status(202)
+    return res.cookie('test-cookie', 'test sample', {
+        path: '/',
+        httpOnly: true,
+        sameSite: 'lax',
+        maxAge: 8760 * 60 * 60 * 1000, // 1 year,
+        secure: true,
+        // signed: true,
+      }).status(202)
       .json({
         message: 'Success',
         data,
       })
-      .cookie('test-cookie', 'test sample', {
-        httpOnly: true,
-        maxAge: 8760 * 60 * 60 * 1000, // 1 year,
-      });
+      
   } catch (err) {
     next(err);
   }
