0b4fbb0f22fd189cec0f240aae09cbc3ff1c074e
Rasel Hossain <rabinarahman@gmail.com>
2022-12-20 16:06:31 +0600

    bug(http-only cookie): test why it's not work on live server

--- backend/service/auth/signInService.js
+++ backend/service/auth/signInService.js
@@ -66,7 +66,7 @@ const signInService = async ({ email, password, res }) => {
       sameSite: 'lax',
       maxAge: 8760 * 60 * 60 * 1000, // 1 year,
       // secure: true,
-      // domain: 'vercel.app',
+      domain: 'vercel.app',
       // domain: 'localhost:3000',
       // signed: true,
     });
