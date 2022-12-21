4ac12a0b790b661bd093a7fc9bb81d6f36e97b9f
Rasel Hossain <rabinarahman@gmail.com>
2022-12-21 15:21:47 +0600

    bug(http-only cookie): test why it's not work on live server

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(
     credentials: true,
     origin: [
       'http://localhost:3000',
-      '.onrender.com',
+      'e-commerce-client-u78t.onrender.com',
       'https://e-commerce-client-u78t.onrender.com',
       'https://e-commerce-full-stack-one.vercel.app',
       'https://accounts.google.com',
