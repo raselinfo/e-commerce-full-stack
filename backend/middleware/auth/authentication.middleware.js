bd7380bcd5e6a0f5c0483a2a2545a0a8ff2e283f
Rasel Hossain <rabinarahman@gmail.com>
2022-12-24 14:10:09 +0600

    bug(authentication.middleware.js):Fix authentication middleware bug

--- backend/middleware/auth/authentication.middleware.js
+++ backend/middleware/auth/authentication.middleware.js
@@ -1,30 +1,49 @@
-const JwtService = require('../../service/jwt/JWT');
-const UserModel = require('../../model/User');
+const JWT = require('../../service/jwt/JWT');
+const UserService = require('../../service/User/UserService.js');
+
+// Documentation
+// This authenticationMiddleware middleware function check is client sent his request with valid accessToken in the headers. and it's also check is there cookie available or not. It's also do some token verification task.
+
 const authenticationMiddleware = async (req, res, next) => {
-  let { refreshToken } = req.cookies;
-  refreshToken = refreshToken?.trim();
-  console.log("I am in AuthMiddleWare")
-  //   if no refresh token
-  if (!refreshToken)
-    return res.status(403).json({ message: 'Please Login/Sign UP' });
+  // is there an access token in header (not=> 403)
+  // check access token validation (not=> 403)
+  // check user exist or not in the database (not=> 403)
+  // check access token role,email and database user role,email (not=>403)
+  // if all the test pass then return next()
+  console.log('authenticationMiddleware');
 
-  // Verify Token
-  let user;
   try {
-    user = await JwtService.verifyRefreshToken(refreshToken);
-  } catch (err) {
-    console.log(err);
-    return res.status(403).json({ message: err.message });
-  }
+    // Access token from header
+    const { authorization } = req.headers;
+    const accessToken = authorization?.split('Bearer')[1]?.trim();
+    //  Refresh Token from req.cookies
+    let { refreshToken } = req.cookies;
+    refreshToken = refreshToken?.trim();
 
-  //   Find User
-  try {
-    const findUser = await UserModel.findById(user._id);
-    if (!findUser && findUser.email !== user.email)
-      return res.status(404).json({ message: 'Please Login/Sign UP' });
+    // if not access token
+    if (!refreshToken) {
+      return res.status(403).json({ message: 'Pleas login first' });
+    }
+    // Token verify
+    const { _id, email, role, name } = await JWT.verifyAccessToken(accessToken);
+
+    const findUser = await UserService.findByProperty('id', _id);
+    // if no found user
+    if (!findUser) {
+      return res.status(401).json({ message: 'UnAuthorize' });
+    }
+    // if user details not match
+    if (
+      findUser.email !== email ||
+      findUser.role !== role ||
+      findUser.name !== name
+    ) {
+      return res.status(401).json({ message: 'UnAuthorize' });
+    }
+
     return next();
   } catch (err) {
-    return res.status(404).json({ message: 'please Login/Sign UP' });
+    return res.status(401).json({ message: err.message });
   }
 };
 module.exports = authenticationMiddleware;
