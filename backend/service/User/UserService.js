25bfb2f536d67973bd7e13a6fba442552d18641b
Rasel Hossain <rabinarahman@gmail.com>
2022-12-24 14:14:59 +0600

    chore(UserService.js): make some document comment usign js-docs

--- backend/service/User/UserService.js
+++ backend/service/User/UserService.js
@@ -1,5 +1,11 @@
 const User = require("../../model/User");
 class UserService {
+  /**
+   * 
+   * @param {id/email} key 
+   * @param {id/email} value 
+   * @returns 
+   */
   static findByProperty(key, value) {
     if (key === "_id") {
       return User.findById(value);
