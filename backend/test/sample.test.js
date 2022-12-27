d0824fe89eadf599a98204d24048f8d7b0d3d06b
Rasel Hossain <rabinarahman@gmail.com>
2022-12-27 22:34:27 +0600

    build(vitest):add vitest

--- backend/test/sample.test.js
+++ backend/test/sample.test.js
@@ -1,13 +1,13 @@
-// import { describe, it, expect } from 'vitest';
-// const addNumber = require('../utils/addNumber.js');
+import { describe, it, expect } from 'vitest';
+const addNumber = require('../utils/addNumber.js');
 
-// describe('Fist Describe', () => {
-//   it('Fist Test', () => {
-//     const result = addNumber(5, 5);
-//     console.log(result);
-//     console.log(result);
-//     console.log(result);
-//     console.log('hello world');
-//     expect(result).toBe(10);
-//   });
-// });
+describe('Fist Describe', () => {
+  it('Fist Test', () => {
+    const result = addNumber(5, 5);
+    console.log(result);
+    console.log(result);
+    console.log(result);
+    console.log('hello world');
+    expect(result).toBe(10);
+  });
+});
