ce058db4006a3cc9ed8a738b408c868da3f70f48
Rasel Hossain <rabinarahman@gmail.com>
2022-12-27 15:34:47 +0600

    feat(Coupon.js):add on property in ShippinPrice Model

--- backend/model/ShippingPrice.js
+++ backend/model/ShippingPrice.js
@@ -1,12 +1,15 @@
 const { Schema, model } = require('mongoose');
-const shippingPriceSchema = new Schema({
-  city: { type: String, unique: true, required: true },
-  amount: { type: Number, required: true },
-  discountCharge: { type: Number, required: true },
-  charge: { type: Number, required: true },
-},{
-  timestamps:true
-});
+const shippingPriceSchema = new Schema(
+  {
+    city: { type: String, unique: true, required: true },
+    amount: { type: Number, required: true },
+    discountCharge: { type: Number, required: true },
+    charge: { type: Number, required: true },
+    on: { type: Boolean, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = model('ShippingCharge', shippingPriceSchema, 'shippingCharge');
-
