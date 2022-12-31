f79fd6e45ecaa6f8bea7e75113766264ed72b0ce
Rasel Hossain <rabinarahman@gmail.com>
2022-12-31 23:04:27 +0600

    feat(App.js): add a private order route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,34 @@
-import "./App.css";
-import { ToastContainer } from "react-toastify";
-import { Routes, Route } from "react-router-dom";
-import Home from "./views/Home";
-import Layout from "./components/Layout";
-import ProductDetails from "./views/ProductDetails";
-import Cart from "./views/Cart";
-import SignIn from "./views/SignIn";
-import Protected from "./Protected Route/Protected";
-import SignUp from "./views/SignUp";
-import VerifyMail from "./views/VerifyMail";
-import AuthProtect from "./Protected Route/AuthProtect";
-import Google from "./components/Google";
-import ForgotPassword from "./views/ForgotPassword";
-import ResetPassword from "./views/ResetPassword";
-import CheckOut from "./views/Checkout";
-
+import './App.css';
+import { ToastContainer } from 'react-toastify';
+import { Routes, Route } from 'react-router-dom';
+import Home from './views/Home';
+import Layout from './components/Layout';
+import ProductDetails from './views/ProductDetails';
+import Cart from './views/Cart';
+import SignIn from './views/SignIn';
+import Protected from './Protected Route/Protected';
+import SignUp from './views/SignUp';
+import VerifyMail from './views/VerifyMail';
+import AuthProtect from './Protected Route/AuthProtect';
+import Google from './components/Google';
+import ForgotPassword from './views/ForgotPassword';
+import ResetPassword from './views/ResetPassword';
+import CheckOut from './views/Checkout';
+import Order from './views/Order';
 const rootPath = window.location.pathname;
 function App() {
   return (
     <>
-      <ToastContainer theme="colored" />
+      <ToastContainer theme='colored' />
       <Layout />
-      {rootPath === "/" && <Google isOpenLoginButton={false} />}
+      {rootPath === '/' && <Google isOpenLoginButton={false} />}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<h1>Hello Admin</h1>} />
-        <Route path="/product/:slug" element={<ProductDetails />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/admin' element={<h1>Hello Admin</h1>} />
+        <Route path='/product/:slug' element={<ProductDetails />} />
+        <Route path='/cart' element={<Cart />} />
         <Route
-          path="/checkout"
+          path='/checkout'
           element={
             <Protected>
               <CheckOut />
@@ -37,7 +37,7 @@ function App() {
         />
 
         <Route
-          path="/signin"
+          path='/signin'
           element={
             <AuthProtect>
               <SignIn />
@@ -45,7 +45,7 @@ function App() {
           }
         />
         <Route
-          path="/signup"
+          path='/signup'
           element={
             <AuthProtect>
               <SignUp />
@@ -53,16 +53,24 @@ function App() {
           }
         />
         <Route
-          path="/Verify/:email/:token"
+          path='/Verify/:email/:token'
           element={
             <AuthProtect>
               <VerifyMail />
             </AuthProtect>
           }
         />
-        <Route path="/forgot/password" element={<ForgotPassword />} />
-        <Route path="/reset/password/:token" element={<ResetPassword />} />
-        <Route path="*" element={<h1>Not Found 404 </h1>} />
+        <Route
+          path='/order/:orderID'
+          element={
+            <Protected>
+              <Order />
+            </Protected>
+          }
+        />
+        <Route path='/forgot/password' element={<ForgotPassword />} />
+        <Route path='/reset/password/:token' element={<ResetPassword />} />
+        <Route path='*' element={<h1>Not Found 404 </h1>} />
       </Routes>
     </>
   );
