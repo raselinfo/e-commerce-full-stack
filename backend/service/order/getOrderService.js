b12b60a6950c87b99a941fe5c68f8d55fa1183cb
Rasel Hossain <rabinarahman@gmail.com>
2023-01-02 18:41:01 +0600

    feat(getOrderService): query order by order id

--- backend/service/order/getOrderService.js
+++ backend/service/order/getOrderService.js
@@ -1,5 +1,16 @@
-const getOrderService=()=>{
-    console.log("get service")
-}
+const Order = require('../../model/Order');
+const getOrderService = async ({ orderID }) => {
+  try {
+    const getOrder = await Order.findById(orderID).select(
+      '-createdAt -updatedAt -__v'
+    );
+    if (!getOrder) {
+      throw new Error('Order Not Found!');
+    }
+    return { result: getOrder };
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
 
-module.exports=getOrderService
+module.exports = getOrderService;
