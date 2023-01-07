d45a534413cc71f1c7494a5644a0825d97edc352
Rasel Hossain <rabinarahman@gmail.com>
2023-01-07 23:32:00 +0600

    feat(useFetch Hook): creat a hook for call all ajax request

---
+++ frontend/src/Hocks/useFetch.js
@@ -0,0 +1,84 @@
+import { useEffect, useReducer } from 'react';
+import formateError from '../utils/formateError';
+import pAxios from './useAxios';
+import publicAxios from '../utils/axios';
+
+// Initial State
+const initialState = {
+  data: null,
+  error: null,
+  loading: true,
+};
+// Actions
+const { REQUEST, SUCCESS, FAIL } = {
+  REQUEST: 'REQUEST',
+  SUCCESS: 'SUCCESS',
+  FAIL: 'FAIL',
+};
+// Reducer
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case REQUEST:
+      return { ...state, loading: true };
+    case SUCCESS:
+      return { ...state, data: payload, loading: false };
+    case FAIL:
+      return { ...state, loading: false, error: payload };
+    default:
+      return state;
+  }
+};
+
+// useFetch Hook
+/**
+ *
+ * @param {string} url
+ * @param {object} options
+ * @returns {object} {data,loading,error}
+ */
+const useFetch = ({
+  url,
+  options = {
+    method: 'get',
+    body: null,
+    private: false,
+  },
+}) => {
+  const [{ data, error, loading }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  const privateAxios = pAxios();
+
+  let axios;
+  if (options.private) {
+    axios = privateAxios;
+  } else {
+    axios = publicAxios;
+  }
+
+  //   Hit api
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: REQUEST });
+        const response = await axios(url, {
+          method: options.method,
+          ...(options.body & { data: options.body }),
+        });
+        dispatch({ type: SUCCESS, payload: response.data });
+      } catch (err) {
+        dispatch({ type: FAIL, payload: formateError(err) });
+      }
+    };
+    if (options.method) {
+      fetchData();
+    } else {
+      dispatch({ type: FAIL, payload: 'Server Error' });
+    }
+  }, [axios, options.body, options.method, url]);
+
+  return { data, error, loading };
+};
+
+export default useFetch;
