a956582c988eb145ef2647c89f2e7c73b525c69b
Rasel Hossain <rabinarahman@gmail.com>
2023-01-07 23:52:57 +0600

    feat(OrderHisoryService.js): sort order documents by descending

--- backend/service/order/getOrderHistoryService.js
+++ backend/service/order/getOrderHistoryService.js
@@ -1,9 +1,9 @@
 const Order = require('../../model/Order');
 const getOrderHistoryService = async ({ userID }) => {
   try {
-    const orders = await Order.find({ user: userID }).select(
-      '_id totalPrice isPaid isDelivered createdAt'
-    );
+    const orders = await Order.find({ user: userID })
+      .sort({ createdAt: -1 })
+      .select('_id totalPrice isPaid isDelivered createdAt');
     return { result: orders };
   } catch (err) {
     throw new Error(err.message);
