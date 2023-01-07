9400e1ee032b053c3778c4faa4d2d70c5c78e826
Rasel Hossain <rabinarahman@gmail.com>
2023-01-07 23:33:14 +0600

    chore(protected routes): improve protected routes query

--- frontend/src/Protected Route/AuthProtect.js
+++ frontend/src/Protected Route/AuthProtect.js
@@ -1,11 +1,11 @@
-import { useContext } from "react";
-import { Store } from "../Store/Store";
-import { Navigate } from "react-router-dom";
+import { useContext } from 'react';
+import { Store } from '../Store/Store';
+import { Navigate } from 'react-router-dom';
 const AuthProtect = ({ children }) => {
   const {
     state: { userInfo },
   } = useContext(Store);
-  return !userInfo.email ? children : <Navigate to="/" />;
+  return !userInfo.email ? children : <Navigate to='/' />;
 };
 
 export default AuthProtect;
