d13e91f4d4ab93a2bf94a535d6e13c3516e63087
Rasel Hossain <rabinarahman@gmail.com>
2023-01-08 19:05:42 +0600

    feat(pagination): add pagination in backend as a service

--- backend/controller/order/getOrderHistoryController.js
+++ backend/controller/order/getOrderHistoryController.js
@@ -1,8 +1,12 @@
 const getOrderHistoryService = require('../../service/order/getOrderHistoryService');
 const getOrderHistoryController = async (req, res, next) => {
   try {
-    const { result } = await getOrderHistoryService({ userID: req.user._id });
-    res.status(200).json({ message: 'OK', data: result });
+    const { result, pagination } = await getOrderHistoryService({
+      userID: req.user._id,
+      page: req.query.page,
+      limit: req.query?.limit,
+    });
+    res.status(200).json({ message: 'OK', data: result, pagination });
   } catch (err) {
     next(err);
   }
--- backend/service/order/getOrderHistoryService.js
+++ backend/service/order/getOrderHistoryService.js
@@ -1,10 +1,24 @@
 const Order = require('../../model/Order');
-const getOrderHistoryService = async ({ userID }) => {
+const getOrderHistoryService = async ({
+  userID,
+  page: p,
+  limit: limitItem,
+}) => {
+  console.log(limitItem);
   try {
+    const page = Number(p) || 1;
+    const limit = Number(limitItem) || 1;
+    const skip = (page - 1) * limit;
+    const items = await Order.countDocuments({ user: userID });
+    const pages = Math.ceil(items / limit);
+
     const orders = await Order.find({ user: userID })
+      .skip(skip)
+      .limit(limit)
       .sort({ createdAt: -1 })
       .select('_id totalPrice isPaid isDelivered createdAt');
-    return { result: orders };
+
+    return { result: orders, pagination: { page: page, pages: pages } };
   } catch (err) {
     throw new Error(err.message);
   }
