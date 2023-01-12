09c6cd6f0c7920c74bb9fb94b2c9954c1e379050
Rasel Hossain <rabinarahman@gmail.com>
2023-01-12 20:26:03 +0600

    feat(testing): add test configuration

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = (uri) => {
   // return mongoose.connect(uri, {
-  return mongoose.connect('mongodb://127.0.0.1:27017/emazona', {
+  return mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
