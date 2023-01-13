a56eb38aba0a162c211b653afd0fa8a5a040d403
Rasel Hossain <rabinarahman@gmail.com>
2023-01-13 23:51:44 +0600

    feat(updateProfileService.js): add image upload and delete previous image functionality

--- backend/service/profile/updateProfileService.js
+++ backend/service/profile/updateProfileService.js
@@ -1,6 +1,11 @@
 const User = require('../../model/User');
 const Password = require('../Password/Password');
-const uploadImage = require('../../utils/imageHandler');
+const { uploadImage, deleteImage } = require('../../utils/imageHandler');
+
+// Documentation
+// updateProfileService update user information
+// if user upload image then it's delete previous image first then then save the new image link in the database
+// if user update his/her password the it's save the hashed password in the local object of the database
 const updateProfileService = async ({ value, user_id }) => {
   try {
     if (!value) {
@@ -15,18 +20,29 @@ const updateProfileService = async ({ value, user_id }) => {
     }
     findUser.name = value.name || findUser.name;
     findUser.email = value.email || findUser.email;
-    let imageResult;
-    if (value.image) {
-      imageResult = await uploadImage(value.image, 'New Image');
+
+    // Upload New Image
+
+    if (value.image && value.image.trim().substring(0, 4) !== 'http') {
+      let imageResult = await uploadImage(value.image, findUser.email);
+      // Deleted previous image
+      if (imageResult.url && findUser?.image?.public_id) {
+        await deleteImage(findUser.image.public_id);
+      }
+      // Save new image in the database
+      findUser.image = {
+        url: imageResult.url,
+        public_id: imageResult.public_id,
+      };
     }
-    findUser.image.url = imageResult.url;
-    findUser.image.public_id = imageResult.public_id;
-    console.log(imageResult.public_id);
+
+    // If password changed
     if (value.password && value.password === value.confirm_password) {
       findUser.local.password = await Password.hash(value.password);
     }
+    // Save all the updated data in the database
     const updatedUser = await findUser.save();
-    console.log('✅✅', updatedUser);
+
     return {
       result: {
         name: updatedUser.name,
