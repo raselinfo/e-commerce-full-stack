4c9a723f3fe5105de147ce6ad030d7408c43cb42
Rasel Hossain <rabinarahman@gmail.com>
2023-01-24 19:09:01 +0600

    chore(Review.js): add user referance in Review Model

--- backend/model/Review.js
+++ backend/model/Review.js
@@ -1,8 +1,12 @@
-const { Schema, model } = require("mongoose");
-const reviewSchema = new Schema({
-  rating: { type: Number },
-  text: { type: String },
-  product: { type: Schema.Types.ObjectId, ref: "Product" },
-});
+const { Schema, model } = require('mongoose');
+const reviewSchema = new Schema(
+  {
+    rating: { type: Number },
+    text: { type: String },
+    product: { type: Schema.Types.ObjectId, ref: 'Product' },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  },
+  { timestamps: true }
+);
 
-module.exports = model("Review", reviewSchema);
+module.exports = model('Review', reviewSchema);
