18bff50f50db2c4de15f444ddd3b774155cb5739
Rasel Hossain <rabinarahman@gmail.com>
2023-01-24 20:06:45 +0600

    fix(formateError.js): add optional chaing

--- frontend/src/utils/formateError.js
+++ frontend/src/utils/formateError.js
@@ -1,11 +1,11 @@
 const formateError = (err) => {
   return err?.response?.status >= 500
     ? `Server Down! 😢 <p>Please give us sometime to fix this issue.OR mail us : example@gmail.com</p>`
-    : err.response.data?.message
-    ? err.response.data?.message
-    : err.message
-    ? err.message
-    : err.response.status > 400
+    : err?.response?.data?.message
+    ? err?.response?.data?.message
+    : err?.message
+    ? err?.message
+    : err?.response?.status > 400
     ? 'Client Error! Please give us sometime to fix this issue.OR mail us : example@gmail.com'
     : 'Something went Wrong! Please give us sometime to fix this issue.OR mail us : example@gmail.com';
 };
