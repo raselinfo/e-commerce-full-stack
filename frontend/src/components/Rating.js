84080cd2a4f450da02281c0e3d5b052a605605a5
Rasel Hossain <rabinarahman@gmail.com>
2023-01-24 19:21:29 +0600

    chore(Rating.js): update Rating Component funtionality

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -12,60 +12,57 @@ const Rating = ({
     return '';
   }
   // Todo: if reviews
-  let rating = reviews.reduce((acc, item) => {
-    if (acc > !item.rating) return acc;
-    return (acc = item.rating);
-  }, 0);
+  let totalReviews = reviews?.numberOfReviews;
   return (
     <div className={`ratting_wrapper ${className}`} style={style && style}>
       <i
         className={
-          rating >= 1
+          reviews.rating >= 1
             ? 'fa-solid fa-star'
-            : rating >= 0.5
+            : reviews.rating >= 0.5
             ? 'fa-solid fa-star-half-stroke'
             : 'fa-regular fa-star'
         }
       ></i>
       <i
         className={
-          rating >= 2
+          reviews.rating >= 2
             ? 'fa-solid fa-star'
-            : rating >= 1.5
+            : reviews.rating >= 1.5
             ? 'fa-solid fa-star-half-stroke'
             : 'fa-regular fa-star'
         }
       ></i>
       <i
         className={
-          rating >= 3
+          reviews.rating >= 3
             ? 'fa-solid fa-star'
-            : rating >= 2.5
+            : reviews.rating >= 2.5
             ? 'fa-solid fa-star-half-stroke'
             : 'fa-regular fa-star'
         }
       ></i>
       <i
         className={
-          rating >= 4
+          reviews.rating >= 4
             ? 'fa-solid fa-star'
-            : rating >= 3.5
+            : reviews.rating >= 3.5
             ? 'fa-solid fa-star-half-stroke'
             : 'fa-regular fa-star'
         }
       ></i>
       <i
         className={
-          rating >= 5
+          reviews.rating >= 5
             ? 'fa-solid fa-star'
-            : rating >= 4.5
+            : reviews.rating >= 4.5
             ? 'fa-solid fa-star-half-stroke'
             : 'fa-regular fa-star'
         }
       ></i>{' '}
       {isShowLength && (
         <span className='text-yellow-500 font-bold'>
-          {reviews.length} Reviews
+          {totalReviews} Reviews
         </span>
       )}
       {caption && <span className='text-yellow-500 font-bold'>{caption}</span>}
